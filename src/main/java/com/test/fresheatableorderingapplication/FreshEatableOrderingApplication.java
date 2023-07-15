b0179b21ca713a4f269324f490bde86ac5e01dae
priyam.guha <priyam.guha@coditas.com>
2023-07-15 17:22:27 +0530

    first commit

---
+++ src/main/java/com/test/fresheatableorderingapplication/FreshEatableOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.test.fresheatableorderingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshEatableOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshEatableOrderingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/fresheatableorderingapplication/FreshEatableOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.fresheatableorderingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshEatableOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
